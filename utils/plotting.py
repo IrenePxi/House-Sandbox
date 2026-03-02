698651e39149e2b3cf3e2762f84b5ffaa2859ec1
IrenePxi <pxi@energy.aau.dk>
2026-03-02 16:32:14 +0100

    Update plotting.py

--- utils/plotting.py
+++ utils/plotting.py
@@ -82,8 +82,8 @@ def plot_period_minute(
         yaxis_title=ytitle,
         hovermode="x unified",
         height=height,
-        margin=dict(l=20, r=20, t=30, b=50),
-        legend=dict(orientation="h", yanchor="top", y=-0.3, xanchor="center", x=0.5)
+        margin=dict(l=20, r=20, t=30, b=80),
+        legend=dict(orientation="h", yanchor="top", y=-0.35, xanchor="center", x=0.5)
     )
 
     return fig
@@ -168,8 +168,8 @@ def plot_period_bar(
         barmode="overlay",
         hovermode="x unified",
         height=height,
-        margin=dict(l=20, r=20, t=30, b=50),
-        legend=dict(orientation="h", yanchor="top", y=-0.3, xanchor="center", x=0.5)
+        margin=dict(l=20, r=20, t=30, b=80),
+        legend=dict(orientation="h", yanchor="top", y=-0.35, xanchor="center", x=0.5)
     )
 
     return fig
